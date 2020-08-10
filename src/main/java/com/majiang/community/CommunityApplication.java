90ab0c5cea45bd4bcc4c3055535ecad6d40940cb
unknown <1163099468@qq.com>
2020-08-10 09:11:05 +0800

    init repo

---
+++ src/main/java/com/majiang/community/CommunityApplication.java
@@ -0,0 +1,14 @@
+package com.majiang.community;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        //        编写主程序，启动springboot应用
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+}
